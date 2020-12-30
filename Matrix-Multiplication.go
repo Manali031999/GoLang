fcbdd9fe25ed82b9e5f67a80599327c91288bebc
Manali031999 <76432416+Manali031999@users.noreply.github.com>
2020-12-30 15:43:13 +0530

    Update Matrix-Multiplication.go

--- Matrix-Multiplication.go
+++ Matrix-Multiplication.go
@@ -1,29 +1,23 @@
 package main
 import "fmt"
 func main(){
-	m1:=[3][2]int{}
-	m2:=[2][3]int{}
-	fmt.Println("Enter 3x2 matrix:")
-	for i:=0;i<3;i++{
-		for j:=0;j<2;j++{
-			fmt.Scan(&m1[i][j])
-		}
+	m1:=[][]int{
+		{1,2},
+		{3,4},
+		{5,6},
 	}
-	fmt.Println("Enter 2x3 matrix:")
-	for i:=0;i<2;i++{
-		for j:=0;j<3;j++{
-			fmt.Scan(&m2[i][j])
-		}
+	m2:=[][]int{
+		{1,2,3},
+		{4,5,6},
+		{7,8,9},
 	}
-	fmt.Println()
 	mul(m1,m2)
 }
-func mul(m1 [3][2]int , m2 [2][3]int){
+func mul(m1 [][]int , m2 [][]int){
 	m3:=[3][3]int{}
-	fmt.Println("Transpose is: ")
+	fmt.Println("Answer is: ")
 	for i:=0;i<3;i++{
 		for j:=0;j<3;j++{
-			m3[i][j]=0
 			for k:=0;k<2;k++{
 				m3[i][j]=m3[i][j]+(m1[i][k]*m2[k][j])
 			}
