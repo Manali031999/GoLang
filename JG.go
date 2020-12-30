9b12928ca63a85d0f2ccfaa990099e3852a7860b
Manali031999 <76432416+Manali031999@users.noreply.github.com>
2020-12-30 16:43:04 +0530

    Update JG.go

--- JG.go
+++ JG.go
@@ -2,7 +2,8 @@ package main
 import "fmt"
 
 func main(){
-	a:=[]int{1,3,2,4,5}
+	a:=[]int{2,4,3,2,2,0,1}
+	//a:=[]int{1}
 	count:=jump(a)
 	if count==0{
 		fmt.Println("Cannot reach")
@@ -12,15 +13,23 @@ func main(){
 }
 
 func jump(a []int) int{
-	var count int
+	var count int 
 	i:=0
 	for i<len(a){
-		if i==len(a)-1 || a[i]==0{
+		if a[i]==0{
 			return count
+		}
+		if i== len(a)-1{
+			return 1
 		}else{
 			b:=a[i]
+			c:=i+b
+				if c >= len(a)-1{
+					count++
+					break
+				}
 			for j:=1;j<=b;j++{
-				if a[i+1]>a[i]{
+				if a[i+1]>=a[i]{
 					i=i+1
 					count++
 					break
