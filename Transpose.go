eec892b2875092b54d0bb92d46551d5a3f405d9e
Manali031999 <76432416+Manali031999@users.noreply.github.com>
2020-12-30 11:55:28 +0530

    Update Transpose.go

--- Transpose.go
+++ Transpose.go
@@ -1,29 +1,34 @@
 package main
 import "fmt" 
-func main() {
-	var i, j, r, c int 
-	var matrix [10][10]int
+
+func transpose(matrix [10][10]int,r,c int){
 	var transpose [10][10]int
-	   	fmt.Print("Enter the number of rows: ")
-		fmt.Scanln(&r) 
-		fmt.Print("Enter the number of columns: ") 
-		fmt.Scanln(&c) 
-		fmt.Println("Enter the matrix elements") 
-		for i = 0; i < r; i++ { 
-			for j = 0; j < c; j++ { 
-				fmt.Scan(&matrix[i][j]) 
-				} 
-			} 
-			for i = 0; i < r; i++ {
-				for j = 0; j < c; j++ { 
-					 transpose[j][i] = matrix[i][j] 
-				} 
-			} 
-			fmt.Println("Transpose of matrix: ") 
-			for i = 0; i < r; i++ { 
-				for j = 0; j < c; j++ { 
-					fmt.Print(transpose[i][j], "\t") 
-				} 
-				fmt.Println() 
-			} 
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ { 
+			transpose[j][i] = matrix[i][j] 
+		} 
+	}
+	fmt.Println("Transpose of matrix: ") 
+	for i:= 0; i < c; i++ { 
+		for j:= 0; j < r; j++ { 
+			fmt.Print(transpose[i][j], "\t") 
+		} 
+		fmt.Println() 
+	}
+}
+
+func main() {
+	var  r, c int 
+	var matrix [10][10]int
+	fmt.Print("Enter the number of rows the matrix: ")
+	fmt.Scanln(&r) 
+	fmt.Print("Enter the number of columns th matrix: ") 
+	fmt.Scanln(&c) 
+	fmt.Println("Enter the matrix elements") 
+	for i := 0; i < r; i++ { 
+		for j:= 0; j < c; j++ { 
+			fmt.Scan(&matrix[i][j]) 
+		} 
+	}  
+	transpose(matrix,r,c)
 }
