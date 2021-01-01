9a988bd72ab34c1375aece68e2d62b3f65695d33
Manali031999 <76432416+Manali031999@users.noreply.github.com>
2021-01-01 17:49:31 +0530

    Update Matrix-Multiplication.go

--- Matrix-Multiplication.go
+++ Matrix-Multiplication.go
@@ -3,24 +3,23 @@ import "fmt"
 func main(){
 	m1:=[][]int{{1,2},{3,4},{5,6},}
 	m2:=[][]int{{1,2,3},{4,5,6},}
-	a:=len(m1)
-	b:=len(m2[0])
-	c:=len(m1[0])
-	if a==b {
-		fmt.Println(mul(m1,m2,c,a))
+	rm1:=len(m1)
+	cm2:=len(m2[0])
+	cm1:=len(m1[0])
+	if rm1==cm2 {
+		fmt.Println("Answer is :",mul(m1,m2,cm1,rm1))
 	}else{
 		fmt.Println("Cannot be Multiplied")
 	}
 }
-func mul(m1  , m2 [][]int , c ,a int ) [][]int{
-	m3:=make([][]int,a)
-	for i:=0 ; i<a ; i++{
-		m3[i]=make([]int,a)
+func mul(m1  , m2 [][]int , cm1 ,rm1 int ) [][]int{
+	m3:=make([][]int,rm1)
+	for i:=0 ; i<rm1 ; i++{
+		m3[i]=make([]int,rm1)
 	}
-	fmt.Println("Answer is: ")
-	for i:=0;i<a;i++{
-		for j:=0;j<a;j++{
-			for k:=0;k<c;k++{
+	for i:=0;i<rm1;i++{
+		for j:=0;j<rm1;j++{
+			for k:=0;k<cm1;k++{
 				m3[i][j]+=(m1[i][k]*m2[k][j])
 			}
 		}
